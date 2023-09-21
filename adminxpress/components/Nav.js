6d679ad4fbb8a7fd7a4a80ab0990d1fae714e4bc
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2023-09-21 10:49:33 +0300

    Google auth process & home UI setup

---
+++ adminxpress/components/Nav.js
@@ -0,0 +1,98 @@
+import Link from "next/link";
+
+export default function Nav() {
+    const inactiveLink = "flex gap-1 p-1"
+    const activeLink = "flex gap-1 p-1 bg-violet-100 text-violet-900 rounded-l-lg"
+    return (
+        <aside className="text-white p-4 pr-0">
+            <Link href={"/"} className="flex gap-1 p-5 mb-4 text-white">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                    />
+                </svg>
+                <span className="">Xpress Admin</span>
+            </Link>
+
+            <nav className="flex flex-col gap-2 ">
+                <Link href={"/"} className={activeLink}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
+                        />
+                    </svg>
+                    Dashboard
+                </Link>
+                <Link href={"/"} className={inactiveLink}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                        />
+                    </svg>
+                    Product
+                </Link>
+                <Link href={"/"} className={inactiveLink}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
+                        />
+                    </svg>
+                    Order
+                </Link>
+                <Link href={"/"} className={inactiveLink}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.5 12a7.5 7.5 0 0015 0m-15 0a7.5 7.5 0 1115 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077l1.41-.513m14.095-5.13l1.41-.513M5.106 17.785l1.15-.964m11.49-9.642l1.149-.964M7.501 19.795l.75-1.3m7.5-12.99l.75-1.3m-6.063 16.658l.26-1.477m2.605-14.772l.26-1.477m0 17.726l-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205L12 12m6.894 5.785l-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864l-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495"
+                        />
+                    </svg>
+                    Setting
+                </Link>
+            </nav>
+        </aside>
+    );
+}
--- adminxpress/pages/index.js
+++ adminxpress/pages/index.js
@@ -1,9 +1,30 @@
-import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/Nav";
 
 export default function Home() {
+    const { data: session } = useSession();
+
+    if (!session) {
+        return (
+            <div className="bg-gradient-to-br from-emerald-400 to-cyan-900 w-screen h-screen flex items-center">
+                <div className="text-center w-full">
+                    <button
+                        onClick={() => signIn("google")}
+                        className="bg-white p-2 rounded-lg px-8"
+                    >
+                        Login with Google
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            hello
+        <div className="bg-violet-900 min-h-screen flex">
+            <Nav />
+            <div className="bg-violet-100 text-cyan-500 mt-2 mr-2 p-4 flex-grow rounded-lg">
+                Logged in {session.user.email}
+            </div>
         </div>
     );
 }
