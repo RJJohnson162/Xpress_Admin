99a3ac524d75de8a8ccb1d1f5b1e133dff17b5dd
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2023-11-23 14:26:49 +0300

    Xpress-Admin

--- tech-xpress/pages/index.js
+++ tech-xpress/pages/index.js
@@ -11,14 +11,14 @@ export default function Home() {
     const [Products, setProducts] = useState([]);
     const [Categories, setCategories] = useState([]);
     useEffect(() => {
-        axios.get("/api/orders").then((response) => {
-            setOrders(response.data);
+        axios.get("/api/categories").then((response) => {
+            setCategories(response.data);
         });
         axios.get("/api/products").then((response) => {
             setProducts(response.data);
         });
-        axios.get("/api/categories").then((response) => {
-            setCategories(response.data);
+        axios.get("/api/orders").then((response) => {
+            setOrders(response.data);
         });
     }, []);
     return (
@@ -40,21 +40,31 @@ export default function Home() {
                         </span>
                     </div>
                 </div>
-                <div className="pic-outline min-w-full my-10 min-h-screen rounded-md px-2 py-4">
+                <div className="pic-outline min-w-full my-10 min-h-4 rounded-md px-2 py-4">
                     <span className="float mb-4 bg-bgGray font-bold flex items-center w-auto flex-col rounded-md">
-                        <h1 className="text-emerald-900 font-bold">Pending orders</h1>
+                        <h1 className="text-emerald-900 font-bold">
+                            Product Categories
+                        </h1>
+                        <h2 className="text-4xl">{Categories.length}</h2>
+                        <h3 className="text-emerald-600">
+                            Variety is our Priority
+                        </h3>
+                    </span>
+                    <span className="float mb-4 bg-bgGray font-bold flex items-center w-auto flex-col rounded-md">
+                        <h1 className="text-emerald-900 font-bold">
+                            Products available
+                        </h1>
+                        <h2 className="text-4xl">{Products.length}</h2>
+                        <h3 className="text-emerald-600">
+                            Commodities in stock
+                        </h3>
+                    </span>
+                    <span className="float mb-4 bg-bgGray font-bold flex items-center w-auto flex-col rounded-md">
+                        <h1 className="text-emerald-900 font-bold">
+                            Pending orders
+                        </h1>
                         <h2 className="text-4xl">{orders.length}</h2>
                         <h3 className="text-emerald-600">Still in progress</h3>
-                    </span>
-                    <span className="float mb-4 bg-bgGray font-bold flex items-center w-auto flex-col rounded-md">
-                        <h1 className="text-emerald-900 font-bold">Product Categories</h1>
-                        <h2 className="text-4xl">{Categories.length}</h2>
-                        <h3 className="text-emerald-600">Variety is our Priority</h3>
-                    </span>
-                    <span className="float mb-4 bg-bgGray font-bold flex items-center w-auto flex-col rounded-md">
-                        <h1 className="text-emerald-900 font-bold">Products available</h1>
-                        <h2 className="text-4xl">{Products.length}</h2>
-                        <h3 className="text-emerald-600">Commodities in stock</h3>
                     </span>
                 </div>
             </div>
